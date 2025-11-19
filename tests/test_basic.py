02595d11955de3e3bac56aa65697613ccc576865
tower_ashley <ashley.dale@utoronto.ca>
2025-11-19 17:06:38 -0500

    update testing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,12 +2,12 @@
 import pytest
 from mlff_attack import __version__
 
-
+@pytest.mark.smoke
 def test_version():
     """Test that version is defined."""
     assert __version__ == "0.1.0"
 
-
+@pytest.mark.smoke
 def test_import():
     """Test that package can be imported."""
     import mlff_attack
